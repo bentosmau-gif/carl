c33b54266cc26ed108224ce52b9752fd07e055ab
Mau-San <bentosmau@gmail.com>
2026-08-02 17:07:34 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (c) 2026 Mau */
+
+/* Example */
+
+
+#include <ckernel.h>
+
+void run_kernel(void) {
+
+    printc("Kernel is running...\n");
+    // Add kernel initialization code here
+}
